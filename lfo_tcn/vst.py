65225b0cbcb10a2025b6988a9b20f2729214064b
christhetree <christhetree@gmail.com>
2023-05-03 00:34:46 +0100

    [cm] Update models

--- lfo_tcn/vst.py
+++ lfo_tcn/vst.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import pathlib
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
 import torch as tr
 import torch.nn as nn
@@ -22,82 +22,89 @@ class EffectModel(nn.Module):
         super().__init__()
         self.sr = sr
         self.model = LSTMEffectModel(in_ch=1, out_ch=1, n_hidden=n_hidden, latent_dim=1)
-        if weights_path and os.path.isfile(weights_path):
+        if weights_path:
+            assert os.path.isfile(weights_path)
             log.info(f"Loading effect model weights: {weights_path}")
             self.model.load_state_dict(tr.load(weights_path))
-        self.prev_phase = 0.0
+        self.prev_phase = tr.tensor(0.0)
 
-    def make_lfo(self, n_samples: int, freq: float, phase: float) -> Tuple[Tensor, float]:
+    def make_argument(self, n_samples: int, freq: float, phase: float) -> Tensor:
         argument = tr.cumsum(2 * tr.pi * tr.full((n_samples,), freq) / self.sr, dim=0) + phase
-        next_phase = argument[-1] % (2 * tr.pi)
-        mod_sig = (tr.cos(argument) + 1.0) / 2.0
-        return mod_sig, next_phase
+        return argument
 
     def forward(self,
                 x: Tensor,
-                lfo_shape: Tensor,
                 lfo_rate: Tensor,
-                lfo_mult: Tensor,
-                stereo_phase_offset: Tensor) -> Tensor:
-        mod_sig, next_phase = self.make_lfo(n_samples=x.size(-1), freq=lfo_rate.item(), phase=self.prev_phase)
-        mod_sig *= lfo_mult
-        mod_sig = mod_sig.view(1, 1, -1)
+                lfo_depth: Tensor,
+                lfo_stereo_phase_offset: Tensor) -> Tensor:
+        arg_l = self.make_argument(n_samples=x.size(-1), freq=lfo_rate.item(), phase=self.prev_phase.item())
+        next_phase = arg_l[-1] % (2 * tr.pi)
         self.prev_phase = next_phase
-        x = self.model(x, mod_sig)
+        arg_r = arg_l + lfo_stereo_phase_offset.item()
+        arg = tr.stack([arg_l, arg_r], dim=0)
+        lfo = (tr.cos(arg) + 1.0) / 2.0
+        lfo *= lfo_depth
+        lfo = lfo.unsqueeze(1)
+        x = self.model(x, lfo)
         return x
 
 
 class EffectModelWrapper(WaveformToWaveformBase):
     def get_model_name(self) -> str:
-        # return "EGFx.phaser"
-        return "EGFx.flanger"
-        # return "EGFx.chorus"
-        # return "Melda.phaser_quasi"
+        return "Melda.phaser_quasi"
         # return "Melda.flanger_quasi"
         # return "Melda.phaser_irregular"
         # return "Melda.flanger_irregular"
+        # return "EGFx.phaser"
+        # return "EGFx.flanger"
+        # return "EGFx.chorus"
 
     def get_model_authors(self) -> List[str]:
         return ["Christopher Mitcheltree"]
 
     def get_model_short_description(self) -> str:
-        return "TBD"
+        return "LFO extraction evaluation model."
 
     def get_model_long_description(self) -> str:
-        return "TBD"
+        return "LFO extraction evaluation model for 'Modulation Extraction for LFO-driven Audio Effects'."
 
     def get_technical_description(self) -> str:
-        return "TBD"
+        return "Wrapper for a simple conditional LSTM that models phaser, flanger, or chorus effects."
 
     def get_technical_links(self) -> Dict[str, str]:
         return {
-            # "Code": "TBD"
+            "Paper": "https://christhetree.github.io/mod_extraction/",
+            "Code": "https://github.com/christhetree/mod_extraction/",
         }
 
     def get_tags(self) -> List[str]:
-        return ["TBD"]
+        return ["lfo", "phaser", "flanger", "chorus"]
 
     def get_model_version(self) -> str:
         return "1.0.0"
 
     def is_experimental(self) -> bool:
-        return False
+        return True
 
     def get_neutone_parameters(self) -> List[NeutoneParameter]:
         return [
-            NeutoneParameter("lfo_shape", "lfo shape [cos, tri]", default_value=0.0),
-            NeutoneParameter("lfo_rate", "lfo rate [0.1 to 5 Hz]", default_value=0.1),
-            NeutoneParameter("lfo_mult", "lfo multiplier [0.0, 2.0]", default_value=0.5),
-            NeutoneParameter("stereo_phase_offset", "stereo phase offset [0.0, 2pi]", default_value=0.0),
+            NeutoneParameter("lfo_rate", "LFO rate [0.1 to 5 Hz]", default_value=0.2),
+            NeutoneParameter("lfo_depth", "LFO depth [0.0, 1.5]", default_value=0.66666666),
+            NeutoneParameter("lfo_stereo_phase_offset", "LFO stereo phase offset [0.0, 2pi]", default_value=0.0),
         ]
 
     @tr.jit.export
+    def get_input_gain_default_value(self) -> float:
+        """[0.0, 1.0] here maps to [-30.0db, +30.0db]"""
+        return 0.4
+
+    @tr.jit.export
     def is_input_mono(self) -> bool:
-        return True
+        return False
 
     @tr.jit.export
     def is_output_mono(self) -> bool:
-        return True
+        return False
 
     @tr.jit.export
     def get_native_sample_rates(self) -> List[int]:
@@ -108,24 +115,23 @@ class EffectModelWrapper(WaveformToWaveformBase):
         return []  # Supports all buffer sizes
 
     def do_forward_pass(self, x: Tensor, params: Dict[str, Tensor]) -> Tensor:
-        lfo_shape = params["lfo_shape"]
         lfo_rate = (params["lfo_rate"] * 4.9) + 0.1
-        lfo_mult = params["lfo_mult"] * 2.0
-        stereo_phase_offset = params["stereo_phase_offset"]
+        lfo_depth = params["lfo_depth"] * 1.5
+        lfo_stereo_phase_offset = params["lfo_stereo_phase_offset"] * 2 * tr.pi
         x = x.unsqueeze(1)
-        x = self.model.forward(x, lfo_shape, lfo_rate, lfo_mult, stereo_phase_offset)
+        x = self.model.forward(x, lfo_rate, lfo_depth, lfo_stereo_phase_offset)
         x = x.squeeze(1)
         return x
 
 
 if __name__ == "__main__":
-    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ph_2_peak__epoch_35_step_95616.pt")
-    weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_fl_2_peak__epoch_20_step_55776.pt")
-    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ch_2_peak__epoch_40_step_108896.pt")
-    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_quasi__epoch_241_step_803440.pt")
+    weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_quasi__epoch_241_step_803440.pt")
     # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_fl_quasi__epoch_207_step_690560.pt")
     # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_irregular__epoch_199_step_664000.pt")
     # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_fl_irregular__epoch_202_step_673960.pt")
+    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ph_2_peak__epoch_35_step_95616.pt")
+    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_fl_2_peak__epoch_20_step_55776.pt")
+    # weights_path = os.path.join(MODELS_DIR, "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ch_2_peak__epoch_40_step_108896.pt")
     model = EffectModel(weights_path=weights_path)
     wrapper = EffectModelWrapper(model)
     root_dir = pathlib.Path(os.path.join(OUT_DIR, "neutone_models", wrapper.get_model_name()))
--- scripts/extract_model_weights.py
+++ scripts/extract_model_weights.py
@@ -13,20 +13,17 @@ log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
 
 if __name__ == "__main__":
     model_dir = MODELS_DIR
-    # model_dir = os.path.join(MODELS_DIR, "final/effect_models/")
-    # model_name = "lfo_2dcnn_io_sa_25_25_no_ch_ln__ph_fl_ch_all_2__idmt_4__epoch_23_step_1920"
-    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ph_2_peak__epoch_35_step_95616"
-    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_fl_2_peak__epoch_20_step_55776"
-    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ch_2_peak__epoch_40_step_108896"
-    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_quasi__epoch_241_step_803440"
+    model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_quasi__epoch_241_step_803440"
     # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_fl_quasi__epoch_207_step_690560"
     # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_ph_irregular__epoch_199_step_664000"
     # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__melda_fl_irregular__epoch_202_step_673960"
+    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ph_2_peak__epoch_35_step_95616"
+    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_fl_2_peak__epoch_20_step_55776"
+    # model_name = "lstm_64__lfo_2dcnn_io_sa_25_25_no_ch_ln__egfx_ch_2_peak__epoch_40_step_108896"
+    pt_module_attr_name = "effect_model"
+
     # model_name = "lfo_2dcnn_io_sa_25_25_no_ch_ln__ph_fl_ch_all_2__idmt_4__epoch_197_step_15840"
-    # model_name = "lfo_2dcnn_io_sa_25_25_no_ch_ln__ph_fl_ch_all_2_both__idmt_4__epoch_196_step_15760"
-    model_name = "a3__lfo_2dcnn_sa_25_25_5x24__ph_rate_idmt_4__epoch_48_step_784"
-    pt_module_attr_name = "model"
-    # pt_module_attr_name = "effect_model"
+    # pt_module_attr_name = "model"
 
     config_path = os.path.join(model_dir, f"{model_name}.yml")
     ckpt_path = os.path.join(model_dir, f"{model_name}.ckpt")
